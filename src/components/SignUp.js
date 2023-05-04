2973421996c85c941118042672da12fdc85077a7
Ket-Kip <ketcybersecure@gmail.com>
2023-05-04 11:26:55 +0300

    added logo to sign up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { NavLink, Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import { useDispatch } from "react-redux";
 import { showNotification, hideNotification } from "./toast/toastActions";
+import logo from "../assets/protracker-final-logo.png"
+
 
 const SignUp = () => {
   const [firstName, setFirstName] = useState("");
@@ -73,13 +75,14 @@ const SignUp = () => {
     <div>
       <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
+        <a
             href="#"
             class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
           >
-            {/* <img class="w-8 h-8 mr-2" src="https://moringaschool.com/wp-content/themes/moringa/public/images/logo.png" alt="logo"/> */}
-            ProTracker
+            <img class="flex h-12 items-center pl-2.5 mb-3  " src={logo} alt="logo"/>
+           
           </a>
+          
           <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
